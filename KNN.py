587ef2c95c5de63a8a0fc58f1f245cea6eb45d7a
MatthewMoye <31222951+MatthewMoye@users.noreply.github.com>
2021-04-12 11:16:45 -0400

    Add files via upload
    use full dataset for KNN and SVM

--- KNN.py
+++ KNN.py
@@ -1,14 +1,15 @@
-import time, random
+"""
+    Python packages required:
+        Tensorflow and Keras (to easily import CIFAR-10)
+        matplotlib and sklearn
+"""
+import random, time
 import numpy as np
 from keras.datasets import cifar10
 from sklearn import svm
 from sklearn import metrics
 from sklearn.neighbors import KNeighborsClassifier
-import matplotlib.pyplot as matplot
-
-# Set seed for replicable results
-np.random.seed(1)
-random.seed(1)
+import matplotlib.pyplot as plt
 
 # Load dataset
 (train_img, train_lbl), (test_img, test_lbl) = cifar10.load_data()
@@ -23,56 +24,34 @@ test_lbl = np.reshape(test_lbl, -1)
 train_img = train_img/255
 test_img = test_img/255
 
-# Define a subset of the training data
-train_subset = 1000
-test_subset = int(train_subset/10)
-
-def data_subset(images, labels, subset_size):
-    label_list = []
-    for i in range(10):
-        label_list += np.array(np.where(labels == i)).tolist()[0][0:subset_size]
-    random.shuffle(label_list)
-    return images[label_list,:], labels[label_list]
-
-# Create a subset for train images
-train_img, train_lbl = data_subset(train_img, train_lbl, train_subset)
-
-# Create a subset for test images
-test_img, test_lbl = data_subset(test_img, test_lbl, test_subset)
-
 # KNN Method
 
 numberNeighbors = []
 times = []
 accuracies = []
 
+# Run KNN on different range of neighbors
 for n in range(1,16):
     timerStart = time.time()
-
     knn = KNeighborsClassifier(n_neighbors=n)
-
     knn.fit(train_img, train_lbl)
     lblPrediction = knn.predict(test_img)
-
     timerEnd = time.time()
-
     timeToRun = timerEnd - timerStart
-
     modelAccuracy = metrics.accuracy_score(test_lbl, lblPrediction)
 
     numberNeighbors.append(n)
     print("Number of neighbors: ", n, "\n")
-
     times.append(timeToRun)
     print("Time to run: ", timeToRun, "\n")
-
     accuracies.append(modelAccuracy)
     print("Accuracy: ", modelAccuracy, "\n")
 
 print("Accuracies: ", accuracies)
 print("Times: ", times)
-matplot.xticks(numberNeighbors)
-fig, axs = matplot.subplots(1, 1, figsize=(10, 10))
+
+plt.xticks(numberNeighbors)
+fig, axs = plt.subplots(1, 1, figsize=(10, 10))
 axs.plot(numberNeighbors, accuracies, color="red", marker="o")
 # Plot settings
 axs.set_ylabel("Accuracy", color="red")
@@ -80,5 +59,5 @@ axs.set(title="KNN accuracy plot", ylabel="Accuracy", xlabel="Neighbors")
 axs2 = axs.twinx()
 axs2.plot(numberNeighbors, times, color="blue", marker="o")
 axs2.set_ylabel("Time to run", color="blue")
-matplot.savefig("KNN.png")
-matplot.clf()
+plt.savefig("KNN.png")
+plt.clf()
--- svm.py
+++ svm.py
@@ -1,50 +1,30 @@
-import random
+"""
+    Python packages required:
+        Tensorflow and Keras (to easily import CIFAR-10)
+        matplotlib and sklearn
+"""
+import random, time
 import numpy as np
-import matplotlib.pyplot as plt
 from keras.datasets import cifar10
 from sklearn import svm
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
 
 
 # Set seed for replicable results
-np.random.seed(1)
 random.seed(1)
 
-"""
-    Load dataset using Keras because its really simple.
-
-    The train dataset has 50,000 images
-    with 50,000 corresponding labels.
-
-    The test dataset has 10,000 images.
-    with 10,000 corresponding labels.
-
-    A single image has size (32,32,3).
-    This means its 32x32 pixels and is an RGB image.
-    Each layer in the third dimension corresponds to 
-    a Red, Green, or Blue color.
-    Each element takes a value between [0,255]
-"""
 (train_img, train_lbl), (test_img, test_lbl) = cifar10.load_data()
 
-"""
-    Here we reshape each image from (32,32,3) to (3072).
-    Labels were reshaped to remove 2nd dimension
-    (50000,1) and (10000,1) changed to (50000,) (10000,)
-"""
+
+#Here we reshape each image from (32,32,3) to (3072).
 train_img = np.reshape(train_img, (train_img.shape[0], -1))
 test_img = np.reshape(test_img, (test_img.shape[0], -1))
 train_lbl = np.reshape(train_lbl, -1)
 test_lbl = np.reshape(test_lbl, -1)
 
-"""
-    Subset size for parameter tuning and final model
-    Subset size is per label type, which there are 10 of
-"""
-parameter_subset = 200
-train_subset = 2000
-
+# Get subset of dataset passed in that has ten class labels
 def data_subset(images, labels, subset_size):
     label_list = []
     for i in range(10):
@@ -53,6 +33,7 @@ def data_subset(images, labels, subset_size):
     # scale data from 0-1
     return (images[label_list,:])/255, labels[label_list]
 
+# Find best C and gamma value
 def parameter_tune(train_img,test_img,train_lbl,test_lbl,subset_size,c_values, gamma_values,nfolds):
     train_img, train_lbl = data_subset(train_img, train_lbl, subset_size)
     test_img, test_lbl = data_subset(test_img, test_lbl, int(subset_size/10))
@@ -66,8 +47,7 @@ def parameter_tune(train_img,test_img,train_lbl,test_lbl,subset_size,c_values, g
                                                      len(gamma_values))
     plt.figure(figsize=(8, 6))
     plt.subplots_adjust(left=.2, right=0.95, bottom=0.15, top=0.95)
-    plt.imshow(scores, interpolation='nearest', cmap=plt.cm.hot
-               )
+    plt.imshow(scores, interpolation='nearest', cmap=plt.cm.hot)
     plt.xlabel('gamma')
     plt.ylabel('C')
     plt.colorbar()
@@ -76,32 +56,34 @@ def parameter_tune(train_img,test_img,train_lbl,test_lbl,subset_size,c_values, g
     plt.title('Validation accuracy')
     plt.show()
     
-
     return grid.best_params_
 
 
-# rbf
-c_values = [0.001,0.01,0.1,1,10,20,50,100,1000,10000,100000,1000000]
-gamma_values = [0.0000001,0.000001,0.00001,0.0001,0.001,0.01,0.1,1,10]
-nfolds = 2
+# Best c and gamma values according parameter_tune fuction
+c = 1
+gamma = 0.01
 
-best_params = parameter_tune(train_img, test_img, train_lbl, test_lbl, parameter_subset, c_values, gamma_values, nfolds)
-c = best_params.get('C')
-gamma = best_params.get('gamma')
+# To get c and gamma again change the find_best_params to True
+find_best_params = False
 
-"""
-    Train better model on the best parameters
-"""
 
-# Subset for train images
-train_img, train_lbl = data_subset(train_img, train_lbl, train_subset) 
+if find_best_params == True:
+    # Subset size for parameter tuning and final model
+    parameter_subset = 200
+    # C and gamma values to test on
+    c_values = [0.001,0.01,0.1,1,10,20,50,100,1000,10000,100000,1000000]
+    gamma_values = [0.0000001,0.000001,0.00001,0.0001,0.001,0.01,0.1,1,10]
+    nfolds = 2
 
-# Subset for test images
-test_img, test_lbl = data_subset(test_img, test_lbl, int(train_subset/10))
+    best_params = parameter_tune(train_img, test_img, train_lbl, test_lbl, parameter_subset, c_values, gamma_values, nfolds)
+    c = best_params.get('C')
+    gamma = best_params.get('gamma')
+
+# Run best c and gamma on full dataset and get results
+start = time.time()
 svmRBF = svm.SVC(kernel='rbf',cache_size=2000)
 svmRBF.fit(train_img,train_lbl)
 test_lbl_predicted_RBF = svmRBF.predict(test_img)
+print(time.time()-start)
 print ("RBF Accuracy: ",accuracy_score(test_lbl,test_lbl_predicted_RBF))
-
-
 
